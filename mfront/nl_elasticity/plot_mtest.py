cfde4f221f1fc1495e8f4b14cae8586033f5789e
marius-duvillard <marius.duvillard@cea.fr>
2026-01-15 15:07:15 +0100

    organize results

--- mfront/nl_elasticity/plot_mtest.py
+++ mfront/nl_elasticity/plot_mtest.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 HERE = Path(__file__).resolve().parent
 file_nn = HERE / "shear_test_nn.res"
+file_nn = HERE / "relax.res"
 file_gt = HERE / "shear_test_gt.res"
 
 
@@ -27,7 +28,7 @@ time2, stress2 = load_data(file_gt)
 
 plt.figure(figsize=(8, 6))
 plt.plot(time1, stress1, label="NN", marker="o")
-plt.plot(time2, stress2, label="GT", marker="x")
+# plt.plot(time2, stress2, label="GT", marker="x")
 plt.xlabel("Shear strain $E_{xy}$")
 plt.ylabel("Shear stress $S_{xy}$ [Pa]")
 plt.title("Shear loading test")
